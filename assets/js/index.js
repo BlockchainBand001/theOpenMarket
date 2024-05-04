0ea929cd34efc62b2a51714eb40417b1eb4cf0ee
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-05-04 19:36:58 +0100

    green market

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,22 @@ document.getElementById('themeBtn').addEventListener('click', function() {
     document.body.classList.toggle('dark-theme');
     this.textContent = this.textContent === 'Dark Theme' ? 'Light Theme' : 'Dark Theme';
 });
+//
+// const sidebar = document.querySelector('.sidebar');
+// const resizeHandle = document.querySelector('.resize-handle');
+// let isResizing = false;
+//
+// resizeHandle.addEventListener('mousedown', function(e) {
+//     isResizing = true;
+//     sidebar.style.width = e.clientX + "px";
+// });
+//
+// window.addEventListener('mousemove', function(e) {
+//     if (isResizing) {
+//         sidebar.style.width  = e.clientX + "px";
+//     }
+// });
+//
+// window.addEventListener('mouseup', function() {
+//     isResizing = false;
+// });
